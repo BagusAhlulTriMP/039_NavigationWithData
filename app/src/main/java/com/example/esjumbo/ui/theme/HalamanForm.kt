fd2491fd3253bc21ed1c3e528f1a2f5c91a11382
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-11-15 16:41:07 +0700

    menambahkan spasi antar teks dan membuat tombol untuk berpindah halaman

--- app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
@@ -2,8 +2,10 @@ package com.example.esjumbo.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,13 +17,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.esjumbo.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-    onSubmitButtonClick: (MutableList<String>) -> Unit
+    onSubmitButtonClicked: (MutableList<String>) -> Unit
 ) {
     var nama by rememberSaveable {
         mutableStateOf("") }
@@ -51,5 +55,10 @@ fun HalamanForm(
             value = alamat,
             onValueChange = {alamat = it },
             label = { Text(text = "Alamat") })
+
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = { onSubmitButtonClicked(listData)}) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
     }
 }
