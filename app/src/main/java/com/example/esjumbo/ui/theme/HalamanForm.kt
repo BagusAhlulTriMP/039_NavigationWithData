4abca7d2a8aa6ac17152e94d005a727673d995f2
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-11-15 16:02:32 +0700

    membuat kolom formulir

--- app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
@@ -1,11 +1,18 @@
 package com.example.esjumbo.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -22,4 +29,12 @@ fun HalamanForm(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(nama, noHp, alamat)
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
 }
