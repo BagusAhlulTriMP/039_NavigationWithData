bff2200aaf08ae1ed931af91b591d203bffa2477
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-11-15 12:23:48 +0700

    menambahkan halaman form

---
+++ app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
@@ -0,0 +1,21 @@
+package com.example.esjumbo.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit
+) {
+    var nama by rememberSaveable {
+        mutableStateOf("") }
+    var noHp by remember {
+        mutableStateOf("") }
+    var alamat by remember {
+        mutableStateOf("") }
+}
