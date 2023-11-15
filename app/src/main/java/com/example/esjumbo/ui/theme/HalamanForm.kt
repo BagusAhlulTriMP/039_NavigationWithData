326dfb4d46a173f00dd90ccb5c1d9827f541ffd7
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-11-15 18:37:02 +0700

    menambahkan perintah kembali pada halamanform

--- app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/ui/theme/HalamanForm.kt
@@ -2,11 +2,14 @@ package com.example.esjumbo.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,48 +20,76 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.esjumbo.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-    onSubmitButtonClicked: (MutableList<String>) -> Unit
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onCancelButtonClicked: () -> Unit
 ) {
     var nama by rememberSaveable {
-        mutableStateOf("") }
+        mutableStateOf("")
+    }
 
     var noHp by remember {
-        mutableStateOf("") }
+        mutableStateOf("")
+    }
 
     var alamat by remember {
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(nama, noHp, alamat)
 
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(16.dp).fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
+        Text(
+            text = "Data Pelanggan",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
         OutlinedTextField(
             value = nama,
-            onValueChange = {nama = it },
+            onValueChange = { nama = it },
             label = { Text(text = "Nama") })
         OutlinedTextField(
             value = noHp,
-            onValueChange = {noHp = it },
+            onValueChange = { noHp = it },
             label = { Text(text = "No Hp") })
         OutlinedTextField(
             value = alamat,
-            onValueChange = {alamat = it },
+            onValueChange = { alamat = it },
             label = { Text(text = "Alamat") })
 
         Spacer(modifier = Modifier.padding(16.dp))
-        Button(onClick = { onSubmitButtonClicked(listData)}) {
-            Text(text = stringResource(id = R.string.btn_submit))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement =
+            Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            OutlinedButton(
+                modifier = Modifier.weight(1f), onClick =
+                onCancelButtonClicked
+            ) {
+                Text(stringResource(R.string.back_button))
+            }
+            Button(modifier = Modifier.weight(1f), onClick = { onSubmitButtonClicked(listData) }) {
+                Text(text = stringResource(id = R.string.next))
+            }
         }
     }
 }
